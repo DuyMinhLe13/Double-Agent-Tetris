ef64eea5a30a04a4638325f5778c43191073c6ac
DuyMinhLe13 <118045868+DuyMinhLe13@users.noreply.github.com>
2023-03-18 15:38:39 +0700

    Update GameSimulation.py

--- GameSimulation.py
+++ GameSimulation.py
@@ -8,7 +8,7 @@ state = env.reset()
 agent_list = [Agent(), Agent()]
 
 while not done:
-    env.render()
+    img = env.render(mode='rgb_array') # img is rgb array, you need to render this or can check my colab notebook in readme file
     action = agent_list[env.game_interface.getCurrentPlayerID()].choose_action(state)
     state, reward, done, _ = env.step(action)
     env.take_turns()
