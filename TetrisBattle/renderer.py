94c5c85d7cc314e2b263d9bc7aa24abe04077ecb
DuyMinhLe13 <118045868+DuyMinhLe13@users.noreply.github.com>
2023-07-12 17:51:59 +0700

    Update renderer.py

--- TetrisBattle/renderer.py
+++ TetrisBattle/renderer.py
@@ -9,6 +9,25 @@ class Renderer(object):
         self.screen = screen
         self.images = images
 
+    def drawAvatar(self, img_path1=None, name1=None, img_path2=None, name2=None, fontsize=40):
+        if img_path1 != None:
+          img1 = pygame.image.load(img_path1)
+          img1 = pygame.transform.scale(img1, (45, 45))
+          self.screen.blit(img1, (38, 25))
+
+        if img_path2 != None:
+          img2 = pygame.image.load(img_path2)
+          img2 = pygame.transform.scale(img2, (45, 45))
+          self.screen.blit(img2, (712, 25))
+        
+        if name1 != None:
+          text1 = pygame.font.SysFont('Comic Sans MS', fontsize, bold=False).render(name1, False, (255, 0, 0))
+          self.screen.blit(text1, (38, 80))
+
+        if name2 != None:
+          text2 = pygame.font.SysFont('Comic Sans MS', fontsize, bold=False).render(name2, False, (255, 0, 0))
+          self.screen.blit(text1, (712, 80))
+
     # this function draws the combo number, return True if sucessfully draw
     def drawCombo(self, tetris, sx, sy):
         
